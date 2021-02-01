f3e8683ad02248128ac402d43b9dbb19504f0558
zallen <47335686+redallen@users.noreply.github.com>
2021-02-01 11:19:19 -0500

    Initial commit

---
+++ config/dev.webpack.config.js
@@ -0,0 +1,20 @@
+const { resolve } = require('path');
+const config = require('@redhat-cloud-services/frontend-components-config');
+const { config: webpackConfig, plugins } = config({
+    rootFolder: resolve(__dirname, '../'),
+    debug: true,
+    https: true
+});
+
+plugins.push(
+    require('@redhat-cloud-services/frontend-components-config/federated-modules')(
+        {
+            root: resolve(__dirname, '../')
+        }
+    )
+);
+
+module.exports = {
+    ...webpackConfig,
+    plugins
+};
