cb39029a8959f1eb20c1a45d794049363c10fed2
redallen <zallen@redhat.com>
2021-02-09 10:32:34 -0500

    replace _ in name

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -7,7 +7,8 @@ const { config: webpackConfig, plugins } = config({
 plugins.push(
     require('@redhat-cloud-services/frontend-components-config/federated-modules')(
         {
-            root: resolve(__dirname, '../')
+            root: resolve(__dirname, '../'),
+            moduleName: 'accessRequests'
         }
     )
 );
