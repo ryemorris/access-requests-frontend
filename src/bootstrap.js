25870c795f47e8aa56dc3701169d82c839492c5f
Filip Hlavac <filip.hlavac01@gmail.com>
2024-01-25 16:33:07 +0100

    Replace ReactDOM.render

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import AppEntry from './AppEntry';
 
 const root = document.getElementById('root');
+root.setAttribute('data-ouia-safe', true);
 
-ReactDOM.render(<AppEntry />, root, () =>
-  root.setAttribute('data-ouia-safe', true)
-);
+ReactDOM.createRoot(root).render(<AppEntry />);
